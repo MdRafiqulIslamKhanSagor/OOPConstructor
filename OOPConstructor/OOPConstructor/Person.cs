937186849359671e7efa9b6990e1cb9db21ec354
Md Rafiqul Islam Khan <mdrafiqulislamkhan@hotmail.com>
2019-10-12 12:44:21 +0600

    Add files via upload

---
+++ OOPConstructor/OOPConstructor/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConstructor
+{
+    class Person
+    {
+        public string firstName;
+        public string middleName;
+        public string lastName;
+
+        public Person(string firstName, string middleName, string lastName):this(firstName,middleName)
+        {
+           
+            this.lastName = lastName;
+            
+        }
+
+        public Person(string firstName,string middleName):this()
+        {
+            this.firstName = firstName;
+            this.middleName = middleName;
+
+        }
+
+        public Person()
+        {
+            int totalBalance = 1000;
+        }
+
+       
+
+        public string GetFullName(string firstName, string middleName, string lastName)
+        {
+
+            this.firstName = firstName;
+            this.middleName = middleName;
+            this.lastName = lastName;
+            string fullName = firstName + " " + middleName + " " + lastName;
+            return fullName;
+        }
+        public string GetFullName(string firstName, string middleName)
+        {
+            this.firstName = firstName;
+            this.middleName = middleName;
+
+            string fullName = firstName + " " + middleName;
+
+            return fullName;
+
+
+        }
+
+        public string GetFullName()
+        {
+            string fullName = firstName;
+            return fullName;
+        }
+    }
+
+   
+}
---
+++ OOPConstructor/OOPConstructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("sagor","khan");
+
+            string fullName = person1.GetFullName();
+
+            Console.WriteLine(fullName);
+        }
+    }
+}
